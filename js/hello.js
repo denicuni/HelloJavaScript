87faa26466568ffbb58e6401aced7c190107ce12
Rullodf <defrancescolorenzo09@gmail.com>
2026-02-14 19:25:27 +0100

    robe per mattia

--- js/hello.js
+++ js/hello.js
@@ -1,47 +1,45 @@
-let b = null;
-console.log(typeof (b));
-b = 3;
-console.log(typeof (b));
-b = "ciao";
-console.log(typeof (b));
+let button = document.querySelector("button")
 
-function example() {
-    return
-    4;
-}
-
-b = document.getElementById("myButton"); // da accesso a id "myButton"
-console.log(b);
-
-let title = document.getElementById("title");
-console.log(title);
-console.log(window.title);
-
-/*b.onclick = function (evt) {
-    alert("Hello, World! da funzione");
-}*/
-
-// b.onclick = (evt) => alert("Hello, World! da funzione");
-
-// b.onclick = (evt) => alert("Ho settato un secondo listener");
-
-b.addEventListener("click", (evt) => {
-    title.textContent = "Questo testo è stato settato da javaScript";
-
-    title.style.color = "orange";
-    title.style.fontSize = "50px";
-
-    let input = document.querySelector("#newInput");
-    console.log(input.value);
-
-    let list = document.querySelector("#myList");
-    const li = document.createElement("li");
-
-    li.textContent = "Nuovo elemento";
-    list.appendChild(li);
-
-    console.log(evt.target.id);
-
+button.addEventListener("click", (event) => {
+	alert("cliccato")
 });
 
-// b.addEventListener("click", (evt) => alert("Hello, World! from second event listener"));
+//funzione che gestisce evento click
+//
+//let event
+//event.clientx = 5
+//event.clienty = 10
+//...
+//...
+//..
+//.
+//
+//for(run : funArray){
+//	run(event)
+//}
+
+function palle() {
+	console.log("palle");
+}
+
+function pasta() {
+	console.log("pasta");
+}
+
+function termostato() {
+	console.log("porcoddio si muore di caldo")
+}
+
+let lista = []
+
+lista.push(palle)
+lista.push(termostato)
+lista.push(pasta)
+
+for (let i = 0; i < lista.length; i++) {
+	lista[i]()
+}
+
+for (fun of lista) {
+	fun()
+}
