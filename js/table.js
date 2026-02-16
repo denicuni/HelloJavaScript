aee7379d478e2557815f5ef1352fb21297a3a2f0
Acocchi29027 <alessandro29027@gmail.com>
2026-02-16 11:51:42 +0100

    Aggiorna il funzionamento del bottone associato alla tabella del file table.js

--- js/table.js
+++ js/table.js
@@ -1,17 +1,39 @@
 const table = document.querySelector("#myTable");
 const tBody = document.querySelector("#myTable tbody");
 const button = document.querySelector("#myTable + button");
+const employees = [{
+    id: 1,
+    name: "Lorenzo",
+    lastname: "Violanti",
+    role: "Senior developer"
+},{
+    id: 2,
+    name: "Mattia",
+    lastname: "Formiconi",
+    role: "CEO"
+},{
+    id: 3,
+    name: "Francesco",
+    lastname: "De Vecchi",
+    role: "Boss Supremo"
+}]
 
 button.addEventListener("click", (evt) => {
-    const tr = document.createElement("tr");
-    const idTd = document.createElement("td");
-    const nameTd = document.createElement("td");
-    const surnameTd = document.createElement("td");
-    idTd.textContent = 1;
-    nameTd.textContent = "Giorgio";
-    surnameTd.textContent = "Vanni";
-    tr.appendChild(idTd);
-    tr.appendChild(nameTd);
-    tr.appendChild(surnameTd);
-    tBody.appendChild(tr);
+    for (let e of employees) {
+        const tr = document.createElement("tr");
+        const idTd = document.createElement("td");
+        const nameTd = document.createElement("td");
+        const surnameTd = document.createElement("td");
+        const roleTd = document.createElement("td");
+        idTd.textContent = e.id;
+        nameTd.textContent = e.name;
+        surnameTd.textContent = e.lastname;
+        roleTd.textContent = e.role;
+        tr.appendChild(idTd);
+        tr.appendChild(nameTd);
+        tr.appendChild(surnameTd);
+        tr.appendChild(roleTd);
+        tBody.appendChild(tr);
+    }
+    button.disabled = true;
 });
