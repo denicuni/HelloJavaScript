b3a38e25265c1f42546e7b03303f1e342e08f066
MelSam06 <m.samoila06@gmail.com>
2026-02-17 12:02:17 +0100

    es

---
+++ js/swapi.js
@@ -0,0 +1,11 @@
+//https://swapi.dev url
+
+/*pagina html, bottone e mi dà ogni film di sky walker 
+con titolo del film e nome del regista 
+con ajax 
+con fetch 
+ognuna(?) ha due operazioni innestate,
+il risultato della seconda dipende dal risultato della prima 
+caricare luke e i suoi figli
+*/
+
